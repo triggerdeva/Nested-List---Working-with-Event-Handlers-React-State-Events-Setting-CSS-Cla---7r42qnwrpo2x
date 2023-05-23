c8b34dac30bb9202130db0e3f2b411bea1374b87
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-05-23 21:20:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -155,7 +155,76 @@ const states = [
 ];
 
 function App() {
-  return <div id="main"></div>;
+  let [cities, setCities] = useState({});
+  let [towns, setTowns] = useState({});
+
+  function stateClicked(e){
+    // console.log("state clicked "+ e.target.className);
+    let index = e.target.id.split("").reverse()[0]-1;
+    let cityName = e.target.className;
+
+    if(cities.hasOwnProperty(cityName)){
+      delete cities[cityName];
+      setCities({...cities});
+      return;
+    }
+
+    setCities({
+      ...cities,
+      [cityName]: states[index].cities,
+    });
+  }
+
+  function cityClicked(e){
+    e.stopPropagation();
+    let townName = e.target.className;
+    // console.log("city clicked "+ townName);
+    let ownIndex = e.target.id.split("").reverse()[0]-1;
+    // console.log(ownIndex);
+    let parentIndex = e.target.value;
+
+    if(towns.hasOwnProperty(townName)){
+      delete towns[townName];
+      setTowns({...towns});
+      return
+    }
+
+    setTowns({
+      ...towns,
+      [townName]: states[parentIndex].cities[ownIndex].towns,
+    })
+      
+  }
+
+  function townClicked(e){
+    e.stopPropagation();
+  }
+  return(
+    <div id="main">
+      <ul>
+        {states.map((state, cityIndex)=>
+          <li onClick={stateClicked} id={`state${cityIndex+1}`} key={cityIndex} className={state.name} >{state.name}
+              {cities.hasOwnProperty(state.name) ?
+                <ul>{cities[state.name].map((city, townIndex)=>
+                  <li onClick={cityClicked} value={cityIndex} id={`city${townIndex+1}`} className={city.name} key={townIndex}>{city.name}
+                    {towns.hasOwnProperty(city.name) ? 
+                      <ul>
+                        {towns[city.name].map((town, index)=>
+                          <li id={`town${index+1}`} onClick={townClicked} key={index}>{town.name}</li>
+                        )}
+                      </ul>
+                    :""}
+                  </li>
+                  )}
+                </ul>
+                : ""
+              }
+          </li>
+        )}
+      </ul>
+
+    </div>
+  );
 }
 
 export default App;
